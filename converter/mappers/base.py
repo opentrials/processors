081f6a2211c8be6e0e4755b1929e8aea01962e1e
roll <roll@post.agency>
2016-03-17 10:55:33 +0300

    fixed nct mapper

--- converter/mappers/base.py
+++ converter/mappers/base.py
@@ -7,6 +7,8 @@ from __future__ import unicode_literals
 from six import add_metaclass
 from abc import ABCMeta, abstractmethod
 
+from .. import indexers
+
 
 # Module API
 
@@ -18,21 +20,62 @@ class Mapper(object):
     def __init__(self, warehouse, database):
         self.__warehouse = warehouse
         self.__database = database
-
-    @property
-    def warehouse(self):
-        """object: Warehouse dataset connection.
-        """
-        return self.__warehouse
-
-    @property
-    def database(self):
-        """object: Database dataset connection.
-        """
-        return self.__database
+        self.__indexers = {
+            'intervention': indexers.Intervention(warehouse),
+            'location': indexers.Location(warehouse),
+            'organisation': indexers.Organisation(warehouse),
+            'person': indexers.Person(warehouse),
+            'problem': indexers.Problem(warehouse),
+            'source': indexers.Source(warehouse),
+            'trial': indexers.Trial(warehouse),
+        }
 
     @abstractmethod
     def map(self):
-        """Map data.
+        """Map data from warehouse to database.
         """
         pass  # pragma: no cache
+
+    def read(self, table):
+        """Read data from warehouse.
+
+        Args:
+            table (object): datastore table object
+
+        Yields:
+            dict: the next item from table
+
+        """
+        offset = 0
+        while True:
+            query = {'_offset': offset, '_limit': bufsize, 'order_by': orderby}
+            count = self.__warehouse[table].find(return_count=True, **query)
+            if not count:
+                break
+            items = self.__warehouse[table].find(**query)
+            offset += bufsize
+            for item in items:
+                yield item
+
+    def index(self, indexer, **kwargs):
+        """Index item.
+
+        Args:
+            indexer (str): indexer name
+
+        Returns:
+            str: identifier
+
+        """
+        return self.__indexers[indexer].index(**kwargs)
+
+    def write(self, table, keys, **data):
+        """Write data to database.
+
+        Args:
+            table (str): table name
+            keys (str[]): keys to decide insert or update
+            data (dict): data to upsert
+
+        """
+        self.__database[table].upsert(data, keys, ensure=False)
--- converter/mappers/nct.py
+++ converter/mappers/nct.py
@@ -6,7 +6,8 @@ from __future__ import unicode_literals
 
 import logging
 
-from .. import base
+from .. import helpers
+from . import base
 logger = logging.getLogger(__name__)
 
 
@@ -14,12 +15,14 @@ class NctMapper(base.Mapper):
 
     # Public
 
+    table = 'nct'
+
     def map(self):
 
         # Map sources
         source_id = map_source()
 
-        for item in helpers.table_read(self.warehouse['nct']):
+        for item in self.read(self.table):
 
             # Map trials
             trial_id = self.map_item_trial(item)
@@ -40,120 +43,136 @@ class NctMapper(base.Mapper):
 
     def map_source(self):
 
-        id = indexers.index_source(self.warehouse,
+        source_id = self.index('source',
             name='nct',
             type='register',
         )
 
-        helpers.upsert(self.database['sources'], {
-            'id': id,
-            'name': 'nct',
-            'type': 'register',
-            'data': {},
-        })
+        self.write('sources', ['id'],
+            id=source_id,
+            name='nct',
+            type='register',
+            data={},
+        )
 
-        return id
+        return source_id
 
     def map_item_trial(self, item):
 
-        id = indexers.index_trial(self.warehouse,
+        trial_id = self.index('trial',
             nct_id=item['nct_id'],
             euctr_id=None,
             isrctn_id=None,
             scientific_title=item['official_title'],
         )
 
-        helpers.upsert(self.database['trials'], {
-            'id': id,
-            'primary_register': 'nct',
-            'primary_id': item['nct_id'],
-            'secondary_ids': {'others': item['secondary_ids'] },
-            'registration_date': item['firstreceived_date'],
-            'public_title': item['brief_title'],
-            'brief_summary': item['brief_summary'] or '',  # TODO: review
-            'scientific_title': item['official_title'],
-            'description': item['detailed_description'],
-            'recruitment_status': item['overall_status'],
-            'eligibility_criteria': item['eligibility'],
-            'target_sample_size': item['enrollment_anticipated'],
-            'first_enrollment_date': item['start_date'],
-            'study_type': item['study_type'],
-            'study_design': item['study_design'],
-            'study_phase': item['phase'],
-            'primary_outcomes': item['primary_outcomes'] or [],
-            'secondary_outcomes': item['secondary_outcomes'] or [],
-        })
+        self.write('trials', ['id'],
+            id=trial_id,
+            primary_register='nct',
+            primary_id=item['nct_id'],
+            secondary_ids={'others': item['secondary_ids'] },
+            registration_date=item['firstreceived_date'],
+            public_title=item['brief_title'],
+            brief_summary=item['brief_summary'] or '',  # TODO: review
+            scientific_title=item['official_title'],
+            description=item['detailed_description'],
+            recruitment_status=item['overall_status'],
+            eligibility_criteria=item['eligibility'],
+            target_sample_size=item['enrollment_anticipated'],
+            first_enrollment_date=item['start_date'],
+            study_type=item['study_type'],
+            study_design=item['study_design'],
+            study_phase=item['phase'],
+            primary_outcomes=item['primary_outcomes'] or [],
+            secondary_outcomes=item['secondary_outcomes'] or [],
+        )
 
-        return id
+        return trial_id
 
     def map_item_record(self, item, trial_id, source_id):
 
-        record_id = item['meta_uuid']
+        self.write('records', ['id'],
+            id=item['meta_id'],
+            source_id=source_id,
+            type='trial',
+            data={'nct_id': item['nct_id']},  # TODO: serialization issue
+        )
 
-        helpers.upsert(db['records'], ['id'], {
-            'id': record_id,
-            'source_id': source_id,
-            'type': 'trial',
-            'data': {'nct_id': item['nct_id']},  # TODO: serialization issue
-        }, auto_id=False)
-
-        helpers.upsert(db['trials_records'], ['trial_id', 'record_id'], {
-            'trial_id': trial_id,
-            'record_id': record_id,
-            'role': 'primary',
-            'context': {},
-        }, auto_id=False)
+        self.write(db['trials_records'], ['trial_id', 'record_id'],
+            trial_id=item['meta_id'],
+            record_id=item['meta_uuid'],
+            role='primary',
+            context={},
+        )
 
     def map_item_problems(self, item, trial_id):
 
         for condition in item['conditions'] or []:
 
-            problem_id = helpers.upsert(db['problems'], ['name', 'type'], {
-                'name': condition,
-                'type': None,
-                'data': {},
-            })
+            problem_id = self.index('problem',
+                name=condition,
+                type=None,
+            )
 
-            helpers.upsert(db['trials_problems'], ['trial_id', 'problem_id'], {
-                'trial_id': trial_id,
-                'problem_id': problem_id,
-                'role': None,
-                'context': {},
-            }, auto_id=False)
+            self.write('problems', ['id'],
+                id=problem_id,
+                name=condition,
+                type=None,
+                data={},
+            )
+
+            self.write('trials_problems', ['trial_id', 'problem_id'],
+                trial_id=trial_id,
+                problem_id=problem_id,
+                role=None,
+                context={},
+            )
 
     def map_item_interventions(self, item, trial_id):
 
         for intervention in item['interventions'] or []:
 
-            intervention_id = helpers.upsert(db['interventions'], ['name', 'type'], {
-                'name': intervention['intervention_name'],
-                'type': None,
-                'data': {},
-            })
+            intervention_id = self.index('intervention',
+                name=intervention['intervention_name'],
+                type=None,
+            )
 
-            helpers.upsert(db['trials_interventions'], ['trial_id', 'intervention_id'], {
-                'trial_id': trial_id,
-                'intervention_id': intervention_id,
-                'role': None,
-                'context': intervention,
-            }, auto_id=False)
+            self.write('interventions', ['id'],
+                id=intervention_id,
+                name=intervention['intervention_name'],
+                type=None,
+                data={},
+            )
+
+            self.write('trials_interventions', ['trial_id', 'intervention_id'],
+                trial_id=trial_id,
+                intervention_id=intervention_id,
+                role=None,
+                context=intervention,
+            )
 
     def map_item_locations(self, item, trial_id):
 
         for location in item['location_countries'] or []:
 
-            location_id = helpers.upsert(db['locations'], ['name', 'type'], {
-                'name': location,
-                'type': 'country',
-                'data': {},
-            })
+            location_id = self.index('location',
+                name=location,
+                type='country',
+            )
 
-            helpers.upsert(db['trials_locations'], ['trial_id', 'location_id'], {
-                'trial_id': trial_id,
-                'location_id': location_id,
-                'role': 'recruitment_countries',
-                'context': {},
-            }, auto_id=False)
+            self.write('locations', ['id'],
+                id=location_id,
+                name=location,
+                type='country',
+                data={},
+            )
+
+            self.write('trials_locations', ['trial_id', 'location_id'],
+                trial_id=trial_id,
+                location_id=location_id,
+                role='recruitment_countries',
+                context={},
+            )
 
     def map_item_organisations(self, item, trial_id):
 
@@ -164,18 +183,24 @@ class NctMapper(base.Mapper):
             if sponsor is None:
                 continue
 
-            organisation_id = helpers.upsert(db['organisations'], ['name'], {
-                'name': sponsor['agency'],
-                'type': None,
-                'data': {},
-            })
+            organisation_id = self.index('organisation',
+                name=sponsor['agency'],
+                type=None,
+            )
 
-            helpers.upsert(db['trials_organisations'], ['trial_id', 'organisation_id'], {
-                'trial_id': trial_id,
-                'organisation_id': organisation_id,
-                'role': 'primary_sponsor',
-                'context': {},
-            }, auto_id=False)
+            self.write('organisations', ['id'],
+                id=organisation_id,
+                name=sponsor['agency'],
+                type=None,
+                data={},
+            )
+
+            self.write('trials_organisations', ['trial_id', 'organisation_id'],
+                trial_id=trial_id,
+                organisation_id=organisation_id,
+                role='primary_sponsor',
+                context={},
+            )
 
     def map_item_persons(self, item, trial_id):
 
@@ -185,18 +210,24 @@ class NctMapper(base.Mapper):
             if person.get('role', None) != 'Principal Investigator':
                 continue
 
-            person_id = helpers.upsert(db['persons'], ['name'], {
-                'name': person['last_name'],
-                'type': None,
-                'data': {},
-            })
+            person_id = self.index('person',
+                name=person['last_name'],
+                type=None,
+            )
 
-            helpers.upsert(db['trials_persons'], ['trial_id', 'person_id'], {
-                'trial_id': trial_id,
-                'person_id': person_id,
-                'role': 'principal_investigator',
-                'context': {},
-            }, auto_id=False)
+            self.write('persons', ['id'],
+                id=person_id,
+                name=person['last_name'],
+                type=None,
+                data={},
+            )
+
+            self.write('trials_persons', ['trial_id', 'person_id'],
+                trial_id=trial_id,
+                person_id=person_id,
+                role='principal_investigator',
+                context={},
+            )
 
 
 if __name__ == '__main__':
