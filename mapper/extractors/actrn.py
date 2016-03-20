a39114ea0468c0b211d2b62e64decb19f00be09c
roll <roll@post.agency>
2016-03-20 13:31:45 +0300

    fixed actrn extractor

--- mapper/extractors/actrn.py
+++ mapper/extractors/actrn.py
@@ -14,154 +14,102 @@ class ActrnExtractor(base.Extractor):
 
     # Public
 
+    direct = True
     table = 'actrn'
-    primary_key = 'trial_id'
 
-    def map(self):
+    def extract_source(self):
 
-        # Map sources
-        source_id = map_source()
+        source = {
+            'name': 'actrn',
+            'type': 'register',
+        }
 
-        for item in helpers.table_read(self.warehouse[self.table]):
+        return source
 
-            # Map trials
-            trial_id = self.map_item_trial(item)
+    def extract_trial(self, item):
 
-            # Map records
-            self.map_item_record(item, trial_id, source_id)
-
-            # Map other entities
-            self.map_item_problems(item, trial_id)
-            self.map_item_interventions(item, trial_id)
-            self.map_item_locations(item, trial_id)
-            self.map_item_organisations(item, trial_id)
-            self.map_item_persons(item, trial_id)
-
-            # Log and sleep
-            logger.debug('Mapped: %s' % item[self.primary_key])
-            time.sleep(0.1)
-
-    def map_source(self):
-
-        source_id = self.index('source',
-            name='actrn',
-            type='register',
-        )
-
-        self.write('sources', ['id'],
-            id=source_id,
-            name='actrn',
-            type='register',
-            data={},
-        )
-
-        return source_id
-
-    def map_item_trial(self, item):
-
-        trial_id = self.index('trial',
-            nct_id=item['nct_id'],
-            euctr_id=None,
-            isrctn_id=None,
-            scientific_title=item['official_title'],
-        )
-
-        self.write('trials', ['id'],
-
-            # General
-            id=trial_id,
-            primary_register='actrn',
-            primary_id=item['trial_id'],
-            secondary_ids={
+        trial = {
+            'nct_id': item['nct_id'],
+            'primary_register': 'actrn',
+            'primary_id': item['trial_id'],
+            'secondary_ids': {
                 'others': item['secondary_ids'],
             },
-            registration_date=item['date_registered'],
-            public_title=item['public_title'],
-            brief_summary=item['brief_summary'],
-            scientific_title=item['scientific_title'],
-            description=None,  # TODO: review
-
-            # Recruitment
-            recruitment_status=item['recruitment_status'],
-            eligibility_criteria={
+            'registration_date': item['date_registered'],
+            'public_title': item['public_title'],
+            'brief_summary': item['brief_summary'],
+            'scientific_title': item['scientific_title'],
+            'description': None,  # TODO: review
+            'recruitment_status': item['recruitment_status'],
+            'eligibility_criteria': {
                 'inclusion': item['key_inclusion_criteria'],
                 'exclusion': item['key_exclusion_criteria'],
             },
-            target_sample_size=item['target_sample_size'],
-            first_enrollment_date=item['anticipated_date_of_first_participant_enrolment'],  # TODO: review
+            'target_sample_size': item['target_sample_size'],
+            'first_enrollment_date': item['anticipated_date_of_first_participant_enrolment'],  # TODO: review
+            'study_type': item['study_type'],
+            'study_design': 'N/A',  # TODO: review
+            'study_phase': item['phase'] or 'N/A',  # TODO: review
+            'primary_outcomes': item['primary_outcomes'] or [],
+            'secondary_outcomes': item['secondary_outcomes'] or [],
+        }
 
-            # Study design
-            study_type=item['study_type'],
-            study_design='N/A',  # TODO: review
-            study_phase=item['phase'] or 'N/A',  # TODO: review
+    def extract_record(self, item):
 
-            # Outcomes
-            primary_outcomes=item['primary_outcomes'] or [],
-            secondary_outcomes=item['secondary_outcomes'] or [],
+        record = {
+            'type': 'trial',
+            'data': {
+                # TODO: item seriliazation issue
+                'actrn_id': item['trial_id'],
+            },
+        }
 
-        )
+        return record
 
-    def map_item_record(self, item, trial_id, source_id):
+    def extract_problems(self, item):
 
-        self.write('records', ['id'],
-            id=item['meta_id'],
-            source_id=source_id,
-            type='trial',
-            data={'actrn_id': item['trial_id']},  # TODO: serialization issue
-        )
-
-        upsert('trials_records', ['trial_id', 'record_id'],
-            trial_id=trial_id,
-            record_id=item['meta_id'],
-            role='primary',
-            context={},
-        )
-
-    def map_item_problems(self, item, trial_id):
         # TODO: item['health_conditions_or_problems_studied'] - free text some time
-        pass
+        problems = []
 
-    def map_item_interventions(self, item, trial_id):
+        return problems
+
+    def extract_interventions(self, item):
+
         # TODO: item['intervention_codes'] - discover
-        pass
+        interventions = []
 
-    def map_item_locations(self, item, trial_id):
+        return intervetions
+
+    def extract_locations(self, item):
+
         # TODO: no recruitment countries
-        pass
+        locations = []
 
-    def map_item_organisations(self, item, trial_id):
+        return locations
 
-        for sponsor in item['sponsors'] or []:
+    def extract_organisations(self, item):
+
+        organisations = []
+
+        for element in item['sponsors'] or []:
 
             # TODO: process item['primary_sponsor']
-            if 'name' not in sponsor:
+            if 'name' not in element:
                 continue
 
-            organisation_id = self.index('organisation',
-                name=sponsor['name'],
-                type=None,
-            )
+            organisations.append({
+                'name': element['name'],
+                'role': 'sponsor',  # TODO: review
+                'context': element,
+            })
 
-            self.write('organisations', ['id'],
-                id=organisation_id,
-                name=sponsor['name'],
-                type=None,
-                data=sponsor,
-            )
+        return organisations
 
-            self.write('trials_organisations', ['trial_id', 'organisation_id'],
-                trial_id=trial_id,
-                organisation_id=organisation_id,
-                role='sponsor',  # TODO: review
-                context={},
-            )
-
-    def map_item_persons(self, item, trial_id):
-
-        # TODO: process item['principal_investigator']
+    def extract_persons(self, item):
 
         persons = []
 
+        # TODO: process item['principal_investigator']
         for role in ['public_queries', 'scientific_queries']:
 
             persons.append({
@@ -170,23 +118,4 @@ class ActrnExtractor(base.Extractor):
                 'context': item[role],
             })
 
-        for person in persons:
-
-            person_id = self.index('person',
-                name=person['name'],
-                type=None,
-            )
-
-            self.write('persons', ['id'],
-                id=person_id,
-                name=person['name'],
-                type=None,
-                data={},
-            )
-
-            self.write('trials_persons', ['trial_id', 'person_id'],
-                trial_id=trial_id,
-                person_id=person_id,
-                role=person['role'],
-                context=person['context'],
-            )
+        return persons
