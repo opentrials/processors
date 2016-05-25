2b6373cce394df97aef8142ba8463e69b7abab14
roll <roll@post.agency>
2016-05-25 16:01:31 +0300

    removed todo from actrn

--- processors/actrn/extractors.py
+++ processors/actrn/extractors.py
@@ -68,17 +68,17 @@ def extract_trial(record):
         'public_title': public_title,
         'brief_summary': record['brief_summary'],
         'scientific_title': record['scientific_title'],
-        'description': None,  # TODO: review
+        'description': record['brief_summary'],
         'recruitment_status': recruitment_status,
         'eligibility_criteria': {
             'inclusion': record['key_inclusion_criteria'],
             'exclusion': record['key_exclusion_criteria'],
         },
         'target_sample_size': record['target_sample_size'],
-        'first_enrollment_date': record['anticipated_date_of_first_participant_enrolment'],  # TODO: review
+        'first_enrollment_date': record['anticipated_date_of_first_participant_enrolment'],
         'study_type': record['study_type'],
-        'study_design': 'N/A',  # TODO: review
-        'study_phase': record['phase'] or 'N/A',  # TODO: review
+        'study_design': None,
+        'study_phase': record['phase'],
         'primary_outcomes': record['primary_outcomes'] or [],
         'secondary_outcomes': record['secondary_outcomes'] or [],
         'gender': gender,
@@ -88,19 +88,20 @@ def extract_trial(record):
 
 
 def extract_conditions(record):
-    # TODO: record['health_conditions_or_problems_studied'] - free text some time
-    conditions = []
+    conditions = [{
+        'name': record['health_conditions_or_problems_studied'],
+        'description': None,
+        'icdcm_code': None,
+    }]
     return conditions
 
 
 def extract_interventions(record):
-    # TODO: record['intervention_codes'] - discover
     interventions = []
     return interventions
 
 
 def extract_locations(record):
-    # TODO: no recruitment countries
     locations = []
     return locations
 
@@ -108,20 +109,18 @@ def extract_locations(record):
 def extract_organisations(record):
     organisations = []
     for element in record['sponsors'] or []:
-        # TODO: process record['primary_sponsor']
         if 'name' not in element:
             continue
         organisations.append({
             'name': element['name'],
             # ---
-            'trial_role': 'sponsor',  # TODO: review
+            'trial_role': 'sponsor',
         })
     return organisations
 
 
 def extract_persons(record):
     persons = []
-    # TODO: process record['principal_investigator']
     for role in ['public_queries', 'scientific_queries']:
         persons.append({
             'name': record[role]['name'],
