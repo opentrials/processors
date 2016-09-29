55ba8340a4cce4e9286f0973220c2e89d70d69d6
Vitor Baptista <vitor@vitorbaptista.com>
2016-09-29 13:38:18 +0100

    [#396] New processor to merge trial and record identifiers
    Fixes opentrials/opentrials#396

---
+++ processors/merge_trial_identifiers/processor.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import datetime
+import logging
+logger = logging.getLogger(__name__)
+
+
+# Module API
+
+def process(conf, conn):
+    """Merge trial identifiers
+    """
+
+    # Prepare
+    query = """
+        WITH records_identifiers AS (
+            SELECT trial_id, ids.key, ids.value
+            FROM records, jsonb_each(identifiers) AS ids
+        ), merged_identifiers AS (
+            SELECT trials.id,
+                trials.identifiers AS trial_identifiers,
+                json_object_agg(key, value)::jsonb AS records_identifiers
+            FROM records_identifiers
+            INNER JOIN trials ON records_identifiers.trial_id = trials.id
+            GROUP BY trials.id
+        )
+        SELECT *
+        FROM merged_identifiers
+        WHERE trial_identifiers != records_identifiers
+    """
+
+    # Execute
+    count = 0
+    failed = 0
+
+    for result in conn['database'].query(query):
+
+        trial_identifiers = result['trial_identifiers']
+        identifiers = dict(trial_identifiers.items() +
+                           result['records_identifiers'].items())
+
+        if sorted(identifiers.items()) == sorted(trial_identifiers.items()):
+            continue
+
+        trial = {
+            'id': result['id'].hex,
+            'identifiers': identifiers,
+            'updated_at': datetime.datetime.utcnow(),
+        }
+
+        try:
+            conn['database']['trials'].update(trial, ['id'])
+            count += 1
+            logger.info("[{}] Trial {} was updated".format(count, trial['id']))
+        except Exception as e:
+            failed += 1
+            logger.warn("[!! {}] Trial {} could not be updated".format(count, trial['id']))
+            logger.debug(e)
+
+    logger.info('{} trials updated'.format(count))
+    logger.info('{} trials failed'.format(failed))
---
+++ tests/processors/merge_trial_identifiers/test_processor.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import mock
+import uuid
+import datetime
+import collections
+import processors.merge_trial_identifiers.processor as processor
+
+
+class TestMergeTrialIdentifiersProcessor(object):
+    @mock.patch('datetime.datetime')
+    def test_updates_trial_with_records_identifiers(self, datetime_mock):
+        query_result = [
+            {
+                'id': uuid.uuid1(),
+                'trial_identifiers': {},
+                'records_identifiers': {'nct': 'NCT11111', 'ictrp': 'ICTRP000000'},
+            },
+        ]
+        db_mock = mock.MagicMock()
+        db_mock['trials'] = mock.Mock()
+        db_mock.query.return_value = query_result
+        datetime_mock.utcnow.return_value = 'the_date'
+
+        conf = {}
+        conn = {'database': db_mock}
+        processor.process(conf, conn)
+
+        expected_trial = {
+            'id': query_result[0]['id'].hex,
+            'identifiers': query_result[0]['records_identifiers'],
+            'updated_at': datetime.datetime.utcnow(),
+        }
+
+        db_mock['trials'].update.assert_called_with(expected_trial, ['id'])
+
+    def test_merges_identifiers(self):
+        query_result = [
+            {
+                'id': uuid.uuid1(),
+                'trial_identifiers': {'nct': 'NCT000000'},
+                'records_identifiers': {'ictrp': 'ICTRP000000'},
+            },
+        ]
+        db_mock = mock.MagicMock()
+        db_mock['trials'] = mock.Mock()
+        db_mock.query.return_value = query_result
+
+        conf = {}
+        conn = {'database': db_mock}
+        processor.process(conf, conn)
+
+        expected_identifiers = {'nct': 'NCT000000', 'ictrp': 'ICTRP000000'}
+        updated_trial, _ = db_mock['trials'].update.call_args[0]
+        assert updated_trial.get('identifiers') == expected_identifiers
+
+    def test_it_uses_records_identifiers_when_there_are_multiple_ids_from_same_source(self):
+        query_result = [
+            {
+                'id': uuid.uuid1(),
+                'trial_identifiers': {'nct': 'NCT00000'},
+                'records_identifiers': {'nct': 'NCT11111'},
+            },
+        ]
+        db_mock = mock.MagicMock()
+        db_mock['trials'] = mock.Mock()
+        db_mock.query.return_value = query_result
+
+        conf = {}
+        conn = {'database': db_mock}
+        processor.process(conf, conn)
+
+        expected_identifiers = query_result[0]['records_identifiers']
+        updated_trial, _ = db_mock['trials'].update.call_args[0]
+        assert updated_trial.get('identifiers') == expected_identifiers
+
+    def test_doesnt_update_identifiers_if_theyre_a_subset_of_the_trials_identifiers(self):
+        query_result = [
+            {
+                'id': uuid.uuid1(),
+                'trial_identifiers': {'nct': 'NCT00000', 'ictrp': 'ICTRP0000000'},
+                'records_identifiers': {'ictrp': 'ICTRP0000000'},
+            },
+        ]
+        db_mock = mock.MagicMock()
+        db_mock['trials'] = mock.Mock()
+        db_mock.query.return_value = query_result
+
+        conf = {}
+        conn = {'database': db_mock}
+        processor.process(conf, conn)
+
+        db_mock['trials'].update.assert_not_called()
+
+    def test_ignores_identifiers_order(self):
+        trial_identifiers = collections.OrderedDict([
+            ('nct', 'NCT000000'),
+            ('drks', 'DRKS0000000'),
+        ])
+        record_identifiers = collections.OrderedDict([
+            ('drks', 'DRKS0000000'),
+            ('nct', 'NCT000000'),
+        ])
+        assert trial_identifiers.items() != record_identifiers.items(), \
+            'Identifiers items must be different unless sorted'
+        query_result = [
+            {
+                'id': uuid.uuid1(),
+                'trial_identifiers': trial_identifiers,
+                'records_identifiers': record_identifiers,
+            },
+        ]
+        db_mock = mock.MagicMock()
+        db_mock['trials'] = mock.Mock()
+        db_mock.query.return_value = query_result
+
+        conf = {}
+        conn = {'database': db_mock}
+        processor.process(conf, conn)
+
+        db_mock['trials'].update.assert_not_called()
